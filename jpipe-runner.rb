33bf9bc8a7d3f8605f8c32b38ab33e6899fab933
Sébastien Mosser <sebastien.mosser@gmail.com>
2025-07-10 15:42:17 -0400

    2nd attempt of python deployment

--- jpipe-runner.rb
+++ jpipe-runner.rb
@@ -9,10 +9,10 @@ class JpipeRunner < Formula
     url "https://github.com/jpipe-mcscert/jpipe-runner/archive/refs/tags/v2.0.0.tar.gz"
     sha256 "38136a9527b930edc56867c20a17c92ae9e99e97d03eed57669fb58fe9bb2013"
 
-    depends_on "python@3.y"
+    depends_on "python@3.10"
 
     def install
-        venv = virtualenv_create(libexec, "python3.y")
+        venv = virtualenv_create(libexec, "python3.10")
         venv.pip_install resources
         venv.pip_install_and_link buildpath
     end
