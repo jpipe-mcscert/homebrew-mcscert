9d6732b39daa3b4daab511c36d404a60600b3d66
Sébastien Mosser <sebastien.mosser@gmail.com>
2025-07-16 10:54:24 -0400

    upgrade python, release java

--- jpipe-runner.rb
+++ jpipe-runner.rb
@@ -7,14 +7,15 @@ class JpipeRunner < Formula
   url "https://github.com/jpipe-mcscert/jpipe-runner/releases/download/v2.0.0b8/jpipe_runner-2.0.0b8.tar.gz"
   sha256 "37b161961d412e68526df7310b9580de320aab485070789cdc30e872559467c3"
 
-  depends_on "python@3.10"
-  depends_on "python-tk@3.10"
+  depends_on "python@3.12"
+  depends_on "python-tk@3.12"
   depends_on "libjpeg-turbo"
   depends_on "freetype"
 
   def install
-    venv = virtualenv_create(libexec, "python3.10")
+    venv = virtualenv_create(libexec, "python3.12")
     venv.pip_install resources
     venv.pip_install_and_link buildpath
   end
+
 end
