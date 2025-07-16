2654773e800edb6f69f8917cbe41b0c28b57a443
Baptiste LACROIX <61122159+BaptisteLacroix@users.noreply.github.com>
2025-07-16 16:36:55 -0400

    Update jpipe-runner@2.0.0b11.rb

--- jpipe-runner@2.0.0b11.rb
+++ jpipe-runner@2.0.0b11.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class JpipeRunner < Formula
+class JpipeRunnerAT2000b11 < Formula
   include Language::Python::Virtualenv
 
   homepage "https://github.com/jpipe-mcscert/jpipe-runner"
