7e2dffbbab973faab6ee5ccd551daefc1201a6e2
Baptiste LACROIX <61122159+BaptisteLacroix@users.noreply.github.com>
2025-07-29 15:10:47 -0400

    Update jpipe-runner.rb

--- jpipe-runner.rb
+++ jpipe-runner.rb
@@ -13,7 +13,20 @@ class JpipeRunner < Formula
   depends_on "freetype"
   depends_on "graphviz"
 
-  
+  resource "networkx" do
+    url "https://files.pythonhosted.org/packages/6c/4f/ccdb8ad3a38e583f214547fd2f7ff1fc160c43a75af88e6aec213404b96a/networkx-3.5.tar.gz"
+    sha256 "d4c6f9cf81f52d69230866796b82afbccdec3db7ae4fbd1b65ea750feed50037"
+  end
+
+  resource "pygraphviz" do
+    url "https://files.pythonhosted.org/packages/66/ca/823d5c74a73d6b8b08e1f5aea12468ef334f0732c65cbb18df2a7f285c87/pygraphviz-1.14.tar.gz"
+    sha256 "c10df02377f4e39b00ae17c862f4ee7e5767317f1c6b2dfd04cea6acc7fc2bea"
+  end
+
+  resource "pyyaml" do
+    url "https://files.pythonhosted.org/packages/54/ed/79a089b6be93607fa5cdaedf301d7dfb23af5f25c398d5ead2525b063e17/pyyaml-6.0.2.tar.gz"
+    sha256 "d584d9ec91ad65861cc08d42e834324ef890a082e591037abe114850ff7bbc3e"
+  end
 
   def install
    ENV["SOURCE_DATE_EPOCH"] = Time.now.to_i.to_s
