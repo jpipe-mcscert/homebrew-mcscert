73c4916144dc8ebc2a454f774b6323257e458e38
Baptiste LACROIX <61122159+BaptisteLacroix@users.noreply.github.com>
2025-07-29 14:27:36 -0400

    Update jpipe-runner.rb

--- jpipe-runner.rb
+++ jpipe-runner.rb
@@ -8,7 +8,7 @@ class JpipeRunner < Formula
   sha256 "13354bbd624bdc93ec6e2bdb0ae33c4b3ef3b68bcd3c2803a8513a86997a6479"
 
   depends_on "python@3.10"
-  depends_on "python-tk@3.10"
+  # depends_on "python-tk@3.10"
   depends_on "libjpeg-turbo"
   depends_on "freetype"
   depends_on "graphviz"
