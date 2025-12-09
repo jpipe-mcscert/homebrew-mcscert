0120b89e44c4aa1499ca1b5c0eac4edcd79e6109
Sébastien Mosser <sebastien.mosser@gmail.com>
2025-12-09 11:44:30 +0100

    using the right tgz template

--- jpipe@1.9.rb
+++ jpipe@1.9.rb
@@ -4,8 +4,8 @@ class JpipeAT19 < Formula
 
     homepage "https://github.com/jpipe-mcscert/jpipe-compiler"
 
-    url "https://github.com/jpipe-mcscert/jpipe-compiler/releases/download/1.9.0/jpipe.jar"
-    sha256 "ebc360d75e7b3d155b59a3fffa151f7038ea36b85fea53f9323b18c78db2823f"
+    url "https://github.com/jpipe-mcscert/jpipe-compiler/releases/download/1.9.0/jpipe-1.9.0.tar.gz"
+    sha256 "570d49cdb4180dbbf429127c98495f68082b71ee6dc9c33f49d4cb21c081a59a"
 
     depends_on "graphviz"
     depends_on "openjdk@25"
